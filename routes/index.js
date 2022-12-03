3b68d5a93f0913868e86260e809f552c1f61a0f4
quang26071999 <108580841+quang26071999@users.noreply.github.com>
2022-12-03 23:20:20 +0700

    aádsd

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const insert = require('./insert');
+
+const multer = require("multer");
+const sizeOf = require("image-size");
+const sharp = require("sharp");
+const alert = require("alert");
 
 mongoose.connect('mongodb+srv://quang123:quang123@cluster0.umb30uq.mongodb.net/');
 
@@ -69,6 +73,50 @@ router.get('/getImg/:page', (req, res, next) => {
         });
 });
 
-insert(router);
+
+
+const Storage = multer.diskStorage({
+    destination: 'public/images',
+    filename: function (req,file,cb){
+        cb(null,Date.now()+file.originalname);
+    }
+});
+const upload = multer({
+    storage: Storage,
+})
+
+router.post('/insertImg',upload.single('img'),function (req, res){
+    const description = req.body.description;
+    const title = req.body.title;
+    const img = req.file.path;
+    let imgSmall;
+    sizeOf(img, async function (err, dimensions) {
+        await sharp(img).resize(Math.round(dimensions.width/2), Math.round(dimensions.height/2)).toFile('./public/images/'+ 're-'+req.file.filename, function(err) {
+            if (err) {
+                console.error('sharp>>>', err)
+            }
+            imgSmall= `http://localhost:3000/images/re-${req.file.filename}`
+            const img1= new Images({
+                description: description,
+                title: title,
+                img: `http://localhost:3000/images/${req.file.filename}`,
+                imgSmall: imgSmall,
+            })
+            img1.save().then(data => {
+                if (data != null) {
+                    alert("Them thanh cong");
+                    res.redirect('insert');
+                } else {
+                    console.log("thất bại");
+                }
+            });
+        })
+
+    });
+})
+
+router.get('/insert', function (req, res){
+    res.render('insert');
+})
 
 module.exports = router;
