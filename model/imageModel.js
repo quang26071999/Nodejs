1d22f060e41ff3d40b9e691da0ce05b23885a5cc
quang26071999 <108580841+quang26071999@users.noreply.github.com>
2022-12-03 22:40:03 +0700

    ácc

--- model/imageModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Image = new mongoose.Schema({
-    title: String,
-    description : String,
-    img: String,
-    imgSmall: String,
-})
-
-module.exports = Images = mongoose.model('Images',Image);
--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-const insert = require('./insert');
-const Images = require('../model/imageModel');
+var express = require('express');
+var router = express.Router();
 
-mongoose.connect('mongodb+srv://quang123:quang123@cluster0.umb30uq.mongodb.net/test');
-
-router.get('/', (req, res, next) => {
-  let perPage = 6; // số lượng sản phẩm xuất hiện trên 1 page
-  let page = req.params.page || 1 ;
-
-  Images
-      .find() // find tất cả các data
-      .skip((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
-      .limit(perPage)
-      .exec((err, products) => {
-        Images.countDocuments((err, count) => { // đếm để tính có bao nhiêu trang
-          if (err) return next(err);
-          res.render('index', {
-            products: products, // sản phẩm trên một page
-            current: page, // page hiện tại
-            pages: Math.ceil(count / perPage) // tổng số các page
-          });
-        });
-      });
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
 });
-
-router.get('/image/:page', (req, res, next) => {
-  let perPage = 6; // số lượng sản phẩm xuất hiện trên 1 page
-  let page = req.params.page ;
-
-  Images
-      .find() // find tất cả các data
-      .skip((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
-      .limit(perPage)
-      .exec((err, products) => {
-        Images.countDocuments((err, count) => { // đếm để tính có bao nhiêu trang
-          if (err) return next(err);
-          res.render('index', {
-            products: products, // sản phẩm trên một page
-            current: page, // page hiện tại
-            pages: Math.ceil(count / perPage) // tổng số các page
-          });
-        });
-      });
-});
-router.get('/getImg/:page', (req, res, next) => {
-  let perPage = 6; // số lượng sản phẩm xuất hiện trên 1 page
-  let page = req.params.page ;
-
-  Images
-      .find() // find tất cả các data
-      .skip((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
-      .limit(perPage)
-      .exec((err, products) => {
-        Images.countDocuments((err, count) => { // đếm để tính có bao nhiêu trang
-          if (err) return next(err);
-          res.json(products);
-        });
-      });
-});
-
-insert(router);
 
 module.exports = router;
