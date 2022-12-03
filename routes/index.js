b0dceecb826d50d7068ac24957161b06f14288e4
quang26071999 <108580841+quang26071999@users.noreply.github.com>
2022-12-03 22:54:11 +0700

    aaaaw

--- routes/index.js
+++ routes/index.js
@@ -14,25 +14,7 @@ const Image = new mongoose.Schema({
 
 const Images = mongoose.model('Images',Image);
 
-router.get('/', (req, res, next) => {
-    let perPage = 6; // số lượng sản phẩm xuất hiện trên 1 page
-    let page = req.params.page || 1 ;
 
-    Images
-        .find() // find tất cả các data
-        .skip((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
-        .limit(perPage)
-        .exec((err, products) => {
-            Images.countDocuments((err, count) => { // đếm để tính có bao nhiêu trang
-                if (err) return next(err);
-                res.render('index', {
-                    products: products, // sản phẩm trên một page
-                    current: page, // page hiện tại
-                    pages: Math.ceil(count / perPage) // tổng số các page
-                });
-            });
-        });
-});
 
 router.get('/image/:page', (req, res, next) => {
     let perPage = 6; // số lượng sản phẩm xuất hiện trên 1 page
